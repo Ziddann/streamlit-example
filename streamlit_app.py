1db7553f692e6df80477756f0c84c59773042edd
Ziddann <116059932+Ziddann@users.noreply.github.com>
2023-12-04 13:16:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,29 +12,29 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+pip install Flask
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+from flask import Flask, render_template
+import houseprice.csv
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+app = Flask(__name__)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+@app.route('/')
+def index():
+    # Baca data dari file CSV
+    data = read_csv('data.csv')
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    # Kirim data ke template HTML
+    return render_template('index.html', data=data)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+def read_csv(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            data.append(row)
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
